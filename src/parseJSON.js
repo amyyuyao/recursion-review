0eede7d5d2c692e455ef84e6f60080979196e965
Yao Yu <amyyuyao.thu@gmail.com>
2022-04-12 17:41:37 -0400

    pass stringifyJSON test

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -4,4 +4,27 @@
 // but you're not, so you'll write it from scratch:
 var parseJSON = function(json) {
   // your code goes here
+
+  if (json.includes('[')) {
+    var arr = [];
+    var sub = json.substring(0, json.length -2)
+    var spl = sub.split(',')
+    if (sub.length === 0) {
+      return [];
+    }
+    spl.forEach(function (item) {
+       arr.push(item)
+    });
+    return arr;
+  } else if (json.includes('{')) {
+    var obj = {};
+    var sub = json.substring(1, json.length -2)
+    var spl = sub.split(',')
+    for (var i = 0; i < spl.length; i++) {
+      var s = spl[i].split(':')
+      obj[s[0]] = s[1]
+    }
+    return obj;
+  }
+
 };
